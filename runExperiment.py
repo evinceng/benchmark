0244714e1593f8c8dbfb16db7fdb42305044bcee
evinceng <aslan.evin@gmail.com>
2018-05-15 15:46:52 +0200

    user dictioary construction fixed and two user interpolation plotted

--- runExperiment.py
+++ runExperiment.py
@@ -16,7 +16,7 @@ from scipy.interpolate import splev
 from interpolate_funs import interpolateBS 
 
 # query variables
-userNames = [ "evina" ] # "evina", "user2",
+userNames = [ "user2", "evina"] # "evina", "user2",
 sessions = [ ] #  "evina2018-05-10 14:33:21.185000", "user22018-05-14 12:06:23.842000",
 projectionFields = [ "userName", "relativeTime", "leftGaze:x", "leftGaze:y" ]
 relativeTime = [ ] # start, end # it will get all times if not provided
@@ -24,7 +24,9 @@ relativeTime = [ ] # start, end # it will get all times if not provided
 # graph variables
 xCoordVariable = "leftGaze:x"
 yCoordVariable = "leftGaze:y"
+timeStampVariable = "relativeTime"
 colorUponVariable = "relativeTime"
+userNameVariable = "userName"
 
 def constructProjectionFileds(getID = False):
     fields = { }
@@ -52,6 +54,24 @@ def constructQuery():
     
     return query
 
+def interpolateAndPlot(xIn, yIn, tIn, tMin, tMax, k, Ts, fCode):
+#    tIn = timeStamps#np.array(timeStamps)
+#    xIn = coordX#np.array(coordX)
+#    yIn = coordY#np.array(coordY)
+    interpolatedXOnTime = interpolateBS(tIn, xIn, tMin, tMax, k, Ts, fCode)
+    interpolatedYOnTime = interpolateBS(tIn, yIn, tMin, tMax, k, Ts, fCode)
+    
+    sampledTime = np.linspace(tMin, tMax, 1000)
+    interpolatedXOnSampledTime = splev(sampledTime, interpolatedXOnTime)
+    interpolatedYOnSampledTime = splev(sampledTime, interpolatedYOnTime)
+    
+    fig = plt.figure()
+    ax = fig.gca() 
+    ax.scatter(xIn, yIn, c=tIn, cmap=cm.seismic)
+    ax.plot(interpolatedXOnSampledTime, interpolatedYOnSampledTime, color='green')
+    ax.legend()
+    plt.show()
+
 if __name__ == "__main__":
     
     host = config.getConfig().get("SERVER", "Host")
@@ -72,51 +92,97 @@ if __name__ == "__main__":
     
     resultList = list(collection.find( query, projection))
     
-    # construct users dict
+    noAction = 0
+    # construct users empty dict-----------------------------------------------
     users = { }
-    
     for userName in userNames:
         users[userName] = { }
         for field in projectionFields:
-            if field != "userName":
-                users[userName][field] = [info[field] for info in resultList]
+            if field != userNameVariable:
+                users[userName][field] = [info[field] for info in resultList if info[userNameVariable] == userName]
                 
-    # end of construct users dict
+#    #fill the users dict, a slower version   
+#    for row in resultList:
+#        currentUser = row[userNameVariable]
+#        for field in projectionFields:
+#            if field != userNameVariable:
+#                users[currentUser][field].append(row[field])
+ 
+    # end of construct users dict----------------------------------------------
     
-    colorUpon = [sensor[colorUponVariable] for sensor in resultList]
-    Coordx = [sensor[xCoordVariable] for sensor in resultList]
-    Coordy = [sensor[yCoordVariable] for sensor in resultList]
-    timeSt = [sensor["relativeTime"] for sensor in resultList]
+#    colorUpon = [sensor[colorUponVariable] for sensor in resultList]
+#    coordX = [sensor[xCoordVariable] for sensor in resultList]
+#    coordY = [sensor[yCoordVariable] for sensor in resultList]
+#    timeStamps = [sensor[timeStampVariable] for sensor in resultList]
     
     #gazeCoordxnpArray = np.array(gazeCoordx)
     #gazeCoordynpArray = np.array(gazeCoordy)
     
 #    fig = plt.figure()
 #    ax = fig.gca() 
-#    ax.scatter(Coordx, Coordy, c=colorUpon, cmap=cm.seismic)
+#    ax.scatter(coordX, coordY, c=colorUpon, cmap=cm.seismic)
 #    ax.legend()
 #    plt.show()
     
-    Ts = 0.1 # Sampling frequency
+    # evina Configuration----------------------------------------------------------
+    userName1 = "evina"
+    Ts = 0.5 # Sampling frequency
     fCode = 1
     k = 3 
-    tMin = 1.2
-    tMax = 6.3
-    tIn = np.array(timeSt)
-    xIn = np.array(Coordx)# np.ones(20) + 0.8*np.random.rand(20)#
-    tck = interpolateBS(tIn, xIn, tMin, tMax, k, Ts, fCode)
+    tMin = 1.6
+    tMax = 5
+    xCoord = users[userName1][xCoordVariable]
+    yCoord = users[userName1][yCoordVariable]
+    timeStamps = users[userName1][timeStampVariable]
+    #interpolateAndPlot(xCoord, yCoord, timeStamps, tMin, tMax, k, Ts, fCode)
     
-    t = np.linspace(1.5, 6.3, 1000)
-    x = splev(t, tck)
+    interpolatedXOnTime = interpolateBS(timeStamps, xCoord, tMin, tMax, k, Ts, fCode)
+    interpolatedYOnTime = interpolateBS(timeStamps, yCoord, tMin, tMax, k, Ts, fCode)
     
-#    plt.figure(1)
-#    plt.title('Check')
-#    plt.plot(timeSt) #), xIn, color='blue')
+    sampledTime = np.linspace(tMin, tMax, 1000)
+    interpolatedXOnSampledTime = splev(sampledTime, interpolatedXOnTime)
+    interpolatedYOnSampledTime = splev(sampledTime, interpolatedYOnTime)
+    
+    # end of evina Configuration---------------------------------------------------
+    
+    # user2 Configuration----------------------------------------------------------
+    userName2 = "user2"
+    TsUser = 0.5 # Sampling frequency
+    fCodeUser = 1
+    kUser = 3 
+    tMinUser = 3 # 1.49
+    tMaxUser = 9 # 10.2
+    xCoordUser = users[userName2][xCoordVariable]
+    yCoordUser = users[userName2][yCoordVariable]
+    timeStampsUser = users[userName2][timeStampVariable]
+    
+    #interpolateAndPlot(xCoordUser, yCoordUser, timeStampsUser, tMinUser, tMaxUser, kUser, TsUser, fCodeUser)
+    interpolatedXOnTimeUser = interpolateBS(timeStampsUser, xCoordUser, tMinUser, tMaxUser, kUser, TsUser, fCodeUser)
+    interpolatedYOnTimeUser = interpolateBS(timeStampsUser, yCoordUser, tMinUser, tMaxUser, kUser, TsUser, fCodeUser)
+    
+    sampledTimeUser = np.linspace(tMinUser, tMaxUser, 1000)
+    interpolatedXOnSampledTimeUser = splev(sampledTimeUser, interpolatedXOnTimeUser)
+    interpolatedYOnSampledTimeUser = splev(sampledTimeUser, interpolatedYOnTimeUser)
+    
+    # end of evina Configuration---------------------------------------------------
+    
+    fig = plt.figure()
+    ax = fig.gca() 
+    ax.scatter(xCoord, yCoord, c=timeStamps, cmap=cm.seismic)
+    ax.scatter(xCoordUser, yCoordUser, c=timeStampsUser, cmap=cm.seismic)
+    ax.plot(interpolatedXOnSampledTime, interpolatedYOnSampledTime, color='green')
+    ax.plot(interpolatedXOnSampledTimeUser, interpolatedYOnSampledTimeUser, color='yellow')
+    ax.legend()
+    plt.show()
+#    plt.figure(2)
+#    plt.title('Gap filled?')
+#    plt.plot(tIn, xIn, 'ro', color='blue')
 #    plt.plot(t, x, color='red')
 #    plt.show()
     
-    plt.figure(2)
-    plt.title('Gap filled?')
-    plt.plot(tIn, xIn, 'ro', color='blue')
-    plt.plot(t, x, color='red')
-    plt.show()
+            
+#    plt.figure(1)
+#    plt.title('Check')
+#    plt.plot(timeStampsUser) #), xIn, color='blue')
+#    #plt.plot(t, x, color='red')
+#    plt.show()
